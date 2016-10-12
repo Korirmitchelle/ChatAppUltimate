9877558fe3b38eac6552f4160a74ee080bef6930
Korirmitchelle <korirmitchelle@gmail.com>
2016-10-12 17:05:23 +0300

    Initial app with added .jar files in lib folder of web and json file for communication between server and client

---
+++ src/info/mitch/chatforum/JSONUtils.java
@@ -0,0 +1,9 @@
+package info.mitch.chatforum;
+
+/**
+ * Created by mitchelle on 10/12/16.
+ */
+public class JSONUtils {
+
+    /*This class contains methods to generate JSON strings those are required to have the communication b/w socket server and clients.*/
+}
